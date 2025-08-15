54ad0168e13d8700091b72394b5d984ebfbb117d
Pradeepto Sarkar <psrcofficial10@gmail.com>
2025-08-15 20:32:53 +0530

    maintenance mode on

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // Toggle this flag to enable/disable maintenance mode across the app
 // Set to false to make the site public again
-export const MAINTENANCE_MODE = false;
+export const MAINTENANCE_MODE = true;
